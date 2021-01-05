506b237a758cb1804d974b24a7921b676e11ad55
wangnanbei <tuanru9166@163.com>
2021-01-05 09:39:11 +0800

    aop

--- src/test/java/com/exception/test/TestApplicationTests.java
+++ src/test/java/com/exception/test/TestApplicationTests.java
@@ -9,6 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootTest(classes = TestApplication.class)
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,9 +23,16 @@ class TestApplicationTests {
 
 
     @Test
-    public void test(){
-        User zhangsan = userLoginService.getUser("zhangsan");
-        System.out.println(zhangsan);
+    public void test() throws ParseException {
+//        User zhangsan = userLoginService.getUser("zhangsan");
+//        System.out.println(zhangsan);
+
+
+        String date = "20201228";
+
+
+        String yyyyMMdd = new SimpleDateFormat("yyyy-MM-dd").format(new SimpleDateFormat("yyyyMMdd").parse(date));
+
     }
 
 
